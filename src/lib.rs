9f4829d2a745d093776271f5ae51b2c44b1c0d86
Icosa-dev <ljcottendev@gmail.com>
2026-03-07 20:58:06 -0500

    Moved to tokio for multithreading

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,23 @@
 use std::{
+    fs::{self, File},
     sync::{Arc, Mutex, mpsc},
     thread,
 };
 
+use anyhow::{Context, Result};
+use serde::{Deserialize, ser::Error};
+
+#[deprecated]
 pub struct ThreadPool {
     workers: Vec<Worker>,
     sender: Option<mpsc::Sender<Job>>,
 }
 
+#[deprecated]
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
+    #[deprecated]
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
@@ -24,12 +31,13 @@ impl ThreadPool {
             workers.push(Worker::new(id, Arc::clone(&reciever)));
         }
 
-        ThreadPool { 
-            workers, 
+        ThreadPool {
+            workers,
             sender: Some(sender),
         }
     }
 
+    #[deprecated]
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
@@ -54,11 +62,13 @@ impl Drop for ThreadPool {
     }
 }
 
+#[deprecated]
 struct Worker {
     id: usize,
     thread: Option<thread::JoinHandle<()>>,
 }
 
+#[deprecated]
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || {
@@ -80,8 +90,28 @@ impl Worker {
         });
 
         Worker {
-            id, 
+            id,
             thread: Some(thread),
         }
     }
+}
+
+#[derive(Deserialize)]
+pub struct ServerConfig {
+    pub ip: String,
+    pub port: u16,
+}
+
+impl ServerConfig {
+    pub fn get_socket(&self) -> String {
+        format!("{}:{}", self.ip, self.port)
+    }
+}
+
+pub fn get_server_config() -> Result<ServerConfig> {
+    let contents = fs::read_to_string("config.toml")
+        .context("Server config couldn't be read")?;
+    let config: ServerConfig = toml::from_str(&contents)
+        .context("Server config could not be deserialized")?;
+    Ok(config)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,97 @@
-use httpserver::ThreadPool;
-use std::{
-    fs, io::{BufReader, prelude::*}, net::{TcpListener, TcpStream}, thread, time::Duration
-};
+// use httpserver::ThreadPool;
+// use std::{
+//     fs, io::{BufReader, prelude::*}, net::{TcpListener, TcpStream}, thread, time::Duration
+// };
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+// fn main() {
+//     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+//     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+//     for stream in listener.incoming() {
+//         let stream = stream.unwrap();
 
-        pool.execute(|| {
-            handle_connection(stream);
+//         pool.execute(|| {
+//             handle_connection(stream);
+//         });
+//     }
+
+//     println!("Shutting down...");
+// }
+
+// fn handle_connection(mut stream: TcpStream) {
+//     let buf_reader = BufReader::new(&stream);
+//     let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+//     let (status_line, filename) = match &request_line[..] {
+//         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "./public/index.html"),
+//         "GET /sleep HTTP/1.1" => {
+//             thread::sleep(Duration::from_secs(5));
+//             ("HTTP/1.1 200 OK", "./public/index.html")
+//         }
+//         _ => ("HTTP/1.1 404 NOT FOUND", "./public/404.html"),
+//     };
+
+//     let contents = fs::read_to_string(filename).unwrap();
+//     let length = contents.len();
+
+//     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+//     stream.write_all(response.as_bytes()).unwrap();
+// }
+
+use std::error::Error;
+use std::time::Duration;
+
+use tokio::fs;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::time::sleep;
+
+use httpserver::get_server_config;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let server_config = get_server_config()?;
+
+    let listener = TcpListener::bind(server_config.get_socket()).await?;
+    // TODO: Add logging instead of print
+    println!("Server running on 127.0.0.1:7878");
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(socket).await {
+                eprintln!("Error handling connection: {}", e);
+            }
         });
     }
-
-    println!("Shutting down...");
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+async fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    let (reader, _) = stream.split();
 
-    let (status_line, filename) = match &request_line[..] {
+    let mut buf_reader = BufReader::new(reader);
+    let mut request_line = String::new();
+
+    if let Err(error) = buf_reader.read_line(&mut request_line).await {
+        return Err(Box::new(error));
+    }
+
+    let (status_line, filename) = match request_line.trim_end() {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "./public/index.html"),
         "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
+            sleep(Duration::from_secs(5)).await;
             ("HTTP/1.1 200 OK", "./public/index.html")
         }
         _ => ("HTTP/1.1 404 NOT FOUND", "./public/404.html"),
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    // TODO: this data should probably go in a struct
+    let contents = fs::read_to_string(filename).await.unwrap(); // this should not unwrap
     let length = contents.len();
-
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).await?;
+    Ok(())
 }
